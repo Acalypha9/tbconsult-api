adba3315e36f506af083f0d618cb873e53b882bc
yosev <yosmisyael7308@gmail.com>
2026-05-28 18:48:06 +0700

    feat: use bcrypt lib directly to fix compatibility issue

--- app/core/security.py
+++ app/core/security.py
@@ -1,18 +1,23 @@
 from datetime import datetime, timedelta, timezone
 from typing import Any
+import bcrypt
 from jose import jwt, JWTError
-from passlib.context import CryptContext
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    """Compare request password with hashed password"""
-    return pwd_context.verify(plain_password, hashed_password)
+    """Validate password hash"""
+    return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
+
 
 def get_password_hash(password: str) -> str:
-    """Hash plain password with bcrypt."""
-    return pwd_context.hash(password)
+    """Hash plain password"""
+    pwd_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed_bytes = bcrypt.hashpw(pwd_bytes, salt)
+
+    return hashed_bytes.decode("utf-8")
+
 
 def create_access_token(data: dict[str, Any]) -> str:
     to_encode = data.copy()
@@ -20,6 +25,7 @@ def create_access_token(data: dict[str, Any]) -> str:
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
     return encoded_jwt
+
 
 def verify_token(token: str) -> dict[str, Any]:
     try:
